1dc23db513656fc94261cda4f5489f294d3f3012
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-08-11 01:52:44 +0500

    weather app js started

--- js/forecast.js
+++ js/forecast.js
@@ -1,1 +1,17 @@
 //=====>> we can use the api that is given in this file<<==========|
+const KEY = 'ff8ac3f3b41140d8dcfdcd2a02e05aca';
+
+
+(async(city) => {
+    const base = `https://api.openweathermap.org/data/2.5/weather`;
+    const query = `?q=${city}&units=metric&appid=${KEY}`;
+  
+    const req = await fetch(base+query);
+    const data = await req.json();
+
+    return data;
+})('London').then((data) => {
+    console.log(data);
+}).catch((err) => {
+    console.log('Something Wrong!');
+})
---
+++ js/main.js
@@ -0,0 +1,7 @@
+const userInput = document.querySelector('#userInput');
+const userBtn = document.querySelector('#userBtn');
+const weatherIcon = document.querySelector('#weathIcon');
+const tempText = document.querySelector('#tempText');
+const cityName = document.querySelector('#cityName');
+const humidityText = document.querySelector('#humidityText');
+const windText = document.querySelector('#windText');
